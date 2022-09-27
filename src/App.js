e0bb233a52bccfd672a894a9bf1cd577486f0faa
vikas11kendre <vikas11kendre@gmail.com>
2022-09-27 11:32:08 +0530

    complete fitness app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import './App.js'
+import React from 'react';
+import {Route, Routes} from 'react-router-dom'
+import {Box} from '@mui/material';
+import './App.css'
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div>Appsss</div>
+    <Box width='400px' sx={{width:{xl:'1488px'}}} m="auto">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/exercise/:id" element={<ExerciseDetail/>} />
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
 
